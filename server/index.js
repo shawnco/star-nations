a02342fc2bbfb69036d077eadf7063edb43f551e
shawnco <shawnc366@gmail.com>
2022-01-23 09:30:10 -0600

    set up back back end

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Add services here
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api/test', (req, res, next) => {
+    res.end(JSON.stringify('Api works!'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+const PORT = 5000;  // but put this in config
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('VERB is now live on port', process.env.PORT || 5000);
+});
